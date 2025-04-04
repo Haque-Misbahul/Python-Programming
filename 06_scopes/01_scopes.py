4fabbacc8995ce1d13b78db9f2885d2949a9d5c9
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-04-05 01:59:19 +0200

    first function working

--- 06_scopes/01_scopes.py
+++ 06_scopes/01_scopes.py
@@ -1,19 +1,19 @@
 username = "chaiaurcode"
 
 def func():
-    # username = "chai"
+    username = "chai"
     print(username)
 
 print(username)
 func()
 
 
-x = 99 
+# x = 99 
 
-def f1():
-    x = 88
-    def f2():
-        print(x)
-    return f2
-myResult = f1()
-myResult()
+# def f1():
+#     x = 88
+#     def f2():
+#         print(x)
+#     return f2
+# myResult = f1()
+# myResult()
