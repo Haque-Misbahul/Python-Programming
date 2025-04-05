075fce2928798ec7123440eb83ec455cdda8e146
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-04-05 02:21:19 +0200

    climbing example

--- 06_scopes/01_scopes.py
+++ 06_scopes/01_scopes.py
@@ -8,12 +8,26 @@ print(username)
 func()
 
 
-# x = 99 
+x = 99 
+# def func2(y):
+#     z = x + y
+#     return z
 
-# def f1():
-#     x = 88
-#     def f2():
-#         print(x)
-#     return f2
-# myResult = f1()
-# myResult()
+# result = func2(1)
+# print(result)
+
+# def func3():
+#     global x
+#     x = 12
+    
+# func3()
+# print(x)
+
+
+def f1():
+    x = 88
+    def f2():
+        print(x)
+    return f2
+myResult = f1()
+myResult()
