5555abe8a3ed4fa09845b2cc41b1bc32d3d31397
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-04-18 22:44:19 +0200

    question 1 solution

---
+++ 07_OOP/solution.py
@@ -0,0 +1,12 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+my_car = Car("Audi", "Q2")
+print(my_car.brand)
+print(my_car.model)
+
+my_new_car = Car("Volkswogen","Tigun")
+print(my_new_car.brand)
+print(my_new_car.model)
