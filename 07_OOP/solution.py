f0729c4fb0cd452af76b515dbbbe329c58e0532e
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-04-18 23:32:16 +0200

    solution 4 added

--- 07_OOP/solution.py
+++ 07_OOP/solution.py
@@ -1,10 +1,13 @@
 class Car:
     def __init__(self, brand, model):
-        self.brand = brand
+        self.__brand = brand
         self.model = model
 
+    def get_brand(self):
+        return self.__brand +" !"
+
     def full_name(self):
-        return f"{self.brand} {self.model}"
+        return f"{self.__brand} {self.model}"
     
 
 class ElectricCar(Car):
@@ -16,16 +19,23 @@ class ElectricCar(Car):
 
 my_tesla = ElectricCar("Tesla", "Model S", "85kwh")
 
-print(my_tesla.full_name())
-print(my_tesla.battery_size)
-print(my_tesla.brand)
-print(my_tesla.model)
+# solution 4
+# # print(my_tesla.brand)
+# print(my_tesla.get_brand())
 
+# solution 3
+# print(my_tesla.full_name())
+# print(my_tesla.battery_size)
+# print(my_tesla.brand)
+# print(my_tesla.model)
+
+# solution 2 
 # my_car = Car("Audi", "Q2")
 # print(my_car.brand)
 # print(my_car.model)
 # print(my_car.full_name())
 
+# solution 1 example 
 # my_new_car = Car("Volkswogen","Tigun")
 # print(my_new_car.brand)
 # print(my_new_car.model)
