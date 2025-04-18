5918fae8f2e1bdaafd163cdc4a7dc50f8638e52e
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-04-19 00:02:10 +0200

    solution 6 added

--- 07_OOP/solution.py
+++ 07_OOP/solution.py
@@ -1,7 +1,11 @@
 class Car:
+
+    total_car = 0
+
     def __init__(self, brand, model):
         self.__brand = brand
         self.model = model
+        Car.total_car += 1
 
     def get_brand(self):
         return self.__brand +" !"
@@ -25,29 +29,35 @@ class ElectricCar(Car):
 
 my_tesla = ElectricCar("Tesla", "Model S", "85kwh")
 
-# solutin 5
+# solutin 6 test
+audi = Car("Audi", "Q2")
+print(audi.fuel_type())
+print(Car.total_car)
+
+
+# solutin 5 test
 # print(my_tesla.fuel_type())
 
 # audi = Car("Audi", "Q2")
 # print(audi.fuel_type())
 
-# solution 4
+# solution 4 test
 # print(my_tesla.brand)
 # print(my_tesla.get_brand())
 
-# solution 3
+# solution 3 test
 # print(my_tesla.full_name())
 # print(my_tesla.battery_size)
 # print(my_tesla.brand)
 # print(my_tesla.model)
 
-# solution 2 
+# solution 2 test
 # my_car = Car("Audi", "Q2")
 # print(my_car.brand)
 # print(my_car.model)
 # print(my_car.full_name())
 
-# solution 1 example 
+# solution 1 test 
 # my_new_car = Car("Volkswogen","Tigun")
 # print(my_new_car.brand)
 # print(my_new_car.model)
