9e2caf0426aaa3dea19a4bd3f86119bc7458ce0f
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-05-21 23:11:30 +0200

    added list all videos functionalitry

--- 09_error_handling/youtube_manager.py
+++ 09_error_handling/youtube_manager.py
@@ -10,10 +10,16 @@ def load_data():
         return []
 
 def save_data_helper(videos):
-    pass
+    with open('youtube.txt', 'w') as file:
+        json.dump(videos, file)
 
 def list_all_videos(videos):
-    pass
+    print("\n")
+    print("*" * 70)
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}. {video['name']}, Duration: {video['time']} ")
+    print("\n")
+    print("*" * 70)
 
 def add_video(videos):
     pass
@@ -27,7 +33,7 @@ def delete_video(videos):
 
 
 def main():
-    videos = []
+    videos = load_data()
     while True:
         print("\n Youtube Manager | choose an option ")
         print("1. List all youtube videos ")
